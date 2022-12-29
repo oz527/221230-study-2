d0f3d69ad9150e7f6c3025502467726100ff86ab
oz527 <118091285+oz527@users.noreply.github.com>
2022-12-30 04:04:10 +0900

    study

---
+++ app.js
@@ -0,0 +1,10 @@
+const player = {
+  name: "seo",
+  sayHello: function (otherPresonName) {
+    console.log("hello! " + otherPresonName + " nice to meet you!");
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("noco");
+player.sayHello("SEO");
